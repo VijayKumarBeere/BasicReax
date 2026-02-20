565dd955f80967909dad2c46212b6b965163c9c7
Vijaykumar <vijayk88259@gmail.com>
2026-02-20 09:54:00 +0530

    Axios Library added.

---
+++ todo/src/Products.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+// import Product from "./Product-card";
+import './Product-card.css'
+function Products(){
+    let [cards, setCards] = useState([]);
+
+    useEffect(
+        () => getProducts(),[]
+    )
+
+    function addProduct(){
+        const product = { title: 'New Product', price: 29.99, description:'', category:'', image:"#", rating:{
+            count:4.2,
+            rate: 4.0
+        } };
+        fetch('https://fakestoreapi.com/products', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(product)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+
+        
+    }
+
+    function removeProduct(id){
+        fetch('https://fakestoreapi.com/products/'+id, {
+            method: 'DELETE'
+            })
+            .then(response => response.json())
+            .then(data => {
+                let details = cards.filter((card)=>card.id!==data.id);
+                console.log(details);
+                setCards(details);
+            });
+    }
+
+    function getProducts(){
+        fetch('https://fakestoreapi.com/products')
+            .then(resp=>resp.json())
+            .then(data=>{
+                console.log(data);
+                setCards(data)
+            });
+        
+    }
+
+    return(
+        <>
+            <div className="container">
+                {
+                    cards.map(product=>{
+                        return (
+                        <div className="row" key={product.id}>
+                            <div className="col-12 col-sm-6 col-md-3 mb-5">
+                                <div className="card h-100 mt-5" style={{width:'18rem'}}>
+                                    <img src={product.image} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h6 className="card-title">{product.title.substring(0,50)}</h6>
+                                        <p className="card-text">{product.description.substring(0,75)}...</p>
+                                        <p>⭐{+ product.rating.rate + '(' + product.rating.count + ')'}</p>
+                                        <p className='text-success fw-bold mt-3'>${product.price}</p>
+                                        <button className="btn btn-danger w-100" onClick={()=>{
+                                            removeProduct(product.id)
+                                        }}>Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>)
+                    })
+                }
+            </div>
+
+            <div className="text-center">
+                <button className="btn btn-primary mt-5" onClick={addProduct}>Add New Product</button>
+            </div>
+        </>
+    )
+}
+
+export default  Products;
--- todo/src/index.js
+++ todo/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './Login';
+// import Login from './Login';
+import Products from './Products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Login></Login>
+    {/* <Login></Login> */}
+    <Products></Products>
   </React.StrictMode>
 );
 
