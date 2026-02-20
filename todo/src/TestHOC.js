a40ba1d84ed4b43b3e67c7044bf3e7e144ef3cc6
Vijaykumar <vijayk88259@gmail.com>
2026-02-20 17:30:01 +0530

    Prop types declaration for a Component

--- todo/src/TestHOC.js
+++ todo/src/TestHOC.js
@@ -1,36 +1,46 @@
-function AuthCheck(Component){
-    let flag =true;
-    return (props)=>{
-        if(flag===true)
-            return(<Component {...props}></Component>)
-        else
-            return (<Login></Login>)
-    }
-}
+import PropTypes from 'prop-types';
+import React from 'react';
+// function AuthCheck(Component){
+//     let flag =true;
+//     return (props)=>{
+//         if(flag===true)
+//             return(<Component {...props}></Component>)
+//         else
+//             return (<Login></Login>)
+//     }
+// }
 
-function User(props){
+function User({username, age}){
     return(
         <>
             <h2>User Component</h2>
-            <h3>User Name:{props.username}</h3>
+            <h3>User Name:{username}</h3>
+            <h4>Age: {age}</h4>
         </>
     )
 }
 
-function Login(){
-    return(
-        <>
-            <h3>Login Component</h3>
-        </>
-    )
-}
+//Prop Type warnings are not showing from react version 19.
+//PropTypes is now considered legacy for runtime validation.
+User.propTypes = {
+    username: PropTypes.string.isRequired,
+    age: PropTypes.number
+};
 
-const Authentication = AuthCheck(User);
+// function Login(){
+//     return(
+//         <>
+//             <h3>Login Component</h3>
+//         </>
+//     )
+// }
+
+// const Authentication = AuthCheck(User);
 
 function HOC(){
     return(
         <>
-            <Authentication username={'john'}></Authentication>
+            <User age = {24}></User>
         </>
     )
 }
